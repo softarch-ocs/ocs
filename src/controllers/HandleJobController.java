4127ebfba8d0c70fe31454cd6303a9057d96c269
afcruzs <bones.felipe@gmail.com>
2016-02-23 19:44:47 -0500

    Handle Job

---
+++ src/controllers/HandleJobController.java
@@ -0,0 +1,36 @@
+package controllers;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+
+@ManagedBean
+@ViewScoped
+public class HandleJobController {
+    //private JobEntity entity;
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void initEdit(Long id ){
+        System.out.println("Init edit");
+        this.id = id;
+        //throw new RuntimeException("kljsdasd");
+    }
+
+
+
+    public boolean isEditing(){
+        System.out.println("Is editing");
+        return id != null;
+    }
+
+
+    /*public JobEntity getEntity() {
+        return entity;
+    }*/
+
+
+
+}
