58712d879b56cc8ec3a090fe8bce966e6df80edd
Daniel Castro <dancas2@gmail.com>
2016-02-28 03:37:52 -0500

    Adding role-based authorization

--- src/controllers/ShowJobsController.java
+++ src/controllers/ShowJobsController.java
@@ -1,15 +1,18 @@
 package controllers;
 
 import data.entities.Job;
+import data.entities.User;
 
 import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import services.UserService;
 import services.jobs.JobServices;
 
 @ManagedBean
 @ViewScoped
-public class ShowJobsController {
+public class ShowJobsController extends BaseController {
     private List<Job> jobs;
 
     public List<Job> getJobs() {
@@ -19,11 +22,20 @@ public class ShowJobsController {
     public void setJobs(List<Job> jobs) {
         this.jobs = jobs;
     }
-
-    public ShowJobsController(){
+    
+    public ShowJobsController(UserService userService) {
+        super(userService);
         
         JobServices jobServices = new JobServices();
         jobs = jobServices.readAllJobs();
-        
+    }
+
+    public ShowJobsController() {
+        this(new UserService());
+    }
+    
+    @PostConstruct
+    public void initialize() {
+        requireRole(User.Role.USER);
     }
 }
