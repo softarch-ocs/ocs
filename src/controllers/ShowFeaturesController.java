0766ec7871741552c35711c02860016f900ea192
hernan940730 <hdolarteb@unal.edu.co>
2016-02-28 02:31:29 +0000

    ShowJobFeatures view and controller added

---
+++ src/controllers/ShowFeaturesController.java
@@ -0,0 +1,66 @@
+package controllers;
+
+import data.entities.Job;
+import data.entities.JobFeature;
+import data.entities.User;
+import java.util.ArrayList;
+
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import services.FeatureServices;
+import services.UserService;
+import services.jobs.JobServices;
+
+@ManagedBean
+@ViewScoped
+public class ShowFeaturesController {
+    
+    private final UserService userService;
+    private final JobServices jobService;
+    private final FeatureServices featureService;
+    
+    private List<JobFeature> userFeatures;
+    private List<JobFeature> jobFeatures;
+
+    public List<JobFeature> getUserFeatures() {
+        return userFeatures;
+    }
+    
+    public List<JobFeature> getJobFeatures() {
+        return jobFeatures;
+    }
+
+    public void setUserFeatures( List<JobFeature> userFeatures ) {
+        this.userFeatures = userFeatures;
+    }
+    
+    public void setJobFeatures( List<JobFeature> jobFeatures ) {
+        this.jobFeatures = jobFeatures;
+    }
+
+    public ShowFeaturesController( ){
+        userService = new UserService();
+        featureService = new FeatureServices();
+        jobService = new JobServices();
+        
+        userFeatures = new ArrayList<>();
+        jobFeatures = new ArrayList<>();
+        
+        User user = userService.getLoggedInUser();
+        
+        if( user != null ){
+            userFeatures = featureService.readFeatures( user );
+        }
+ 
+    }
+    
+    public void initJobFeatures( Long id ){
+         if( id != null ){
+            Job job = jobService.readJob( Integer.parseInt( id + "" ) );
+            if( job != null ){
+                jobFeatures = featureService.readFeatures( job );
+            }
+        }
+    }
+}
