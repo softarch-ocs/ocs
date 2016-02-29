e368e9853827f82067442f630ec80e4360ec7c02
Felipe Cruz <bones.felipe@gmail.com>
2016-02-28 19:08:28 -0500

    Security restrictions added in StatisticsController.

--- src/controllers/StatisticsController.java
+++ src/controllers/StatisticsController.java
@@ -5,20 +5,24 @@ import DTO.statistics.CountJobsByAgeDTO;
 import DTO.statistics.CountJobsByAreaDTO;
 import DTO.statistics.CountJobsByGenderDTO;
 import com.sun.xml.rpc.processor.generator.nodes.JaxRpcMappingTagNames;
+import data.entities.User;
 import java.util.List;
 import java.util.Map;
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import services.UserService;
 import services.jobs.StatisticsService;
 
 @ManagedBean
 @ViewScoped
-public class StatisticsController {
+public class StatisticsController extends BaseController {
     private StatisticsService statisticsService;
     private String areaData, genderData;
     private String yearData;
     
-    public StatisticsController(StatisticsService statisticsService) {
+    public StatisticsController(StatisticsService statisticsService, UserService userService) {
+        super(userService);
         if( statisticsService == null ){
             throw new IllegalArgumentException("statisticsServices");
         }
@@ -27,10 +31,15 @@ public class StatisticsController {
     }
 
     public StatisticsController() {
-        this(new StatisticsService());
+        this(new StatisticsService(),new UserService());
         areaData =  CountJobsByAreaDTO.dictionaryToString( statisticsService.getAreaData() );
         genderData = CountJobsByGenderDTO.dictionaryToString( statisticsService.getGenderData() );
         yearData =  CountJobsByAgeDTO.dictionaryToString( statisticsService.getAgeData() );
+    }
+    
+    @PostConstruct
+    public void initialize() {
+        requireRole(User.Role.ADMIN);
     }
 
     public String getYearData() {
