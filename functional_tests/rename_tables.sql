c99845fa2b766b659d7ad59a1e007a804cf177fc
hernan940730 <hdolarteb@unal.edu.co>
2016-02-29 00:13:11 +0000

    Features Test Cases added

---
+++ functional_tests/rename_tables.sql
@@ -0,0 +1,8 @@
+RENAME TABLE `job` TO `Job`;
+RENAME TABLE `job_jobfeatures` TO `Job_jobFeatures`;
+RENAME TABLE `jobarea` TO `JobArea`;
+RENAME TABLE `jobfeature` TO `JobFeature`;
+RENAME TABLE `user` TO `User`;
+RENAME TABLE `user_jobfeatures` TO `User_jobFeatures`;
+RENAME TABLE `usersjobs` TO `UsersJobs`;
+
