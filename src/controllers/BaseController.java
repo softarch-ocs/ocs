8b1bda58c818673ac7024de2fee6735e8e6f3f1e
Daniel Castro <dancas2@gmail.com>
2016-02-28 23:22:10 -0500

    Fixed post-login redirection when there are query parameters

--- src/controllers/BaseController.java
+++ src/controllers/BaseController.java
@@ -4,6 +4,7 @@ import data.entities.User;
 import java.io.IOException;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
 import services.UserService;
 
 public abstract class BaseController {
@@ -26,7 +27,8 @@ public abstract class BaseController {
         if (user == null || user.getRole().compareTo(role) < 0) {
             FacesContext context = FacesContext.getCurrentInstance();
             ExternalContext externalContext = context.getExternalContext();
-            String currentPath = externalContext.getRequestServletPath();
+            HttpServletRequest request = (HttpServletRequest)externalContext.getRequest();
+            String currentPath = request.getServletPath() + "?" + request.getQueryString();
             
             // Flash messages won't work in this case, so let's use session
             externalContext.getSessionMap().put("POST_LOGIN_REDIRECT", currentPath);
