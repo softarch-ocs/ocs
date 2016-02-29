e75c306a8b94a7850b7585fb614d0bb17dbe4689
Daniel Castro <dancas2@gmail.com>
2016-02-28 21:40:29 -0500

    Adding UserJobs CRUD views

--- src/services/jobs/UserJobRelationServices.java
+++ src/services/jobs/UserJobRelationServices.java
@@ -3,38 +3,88 @@ package services.jobs;
 import data.dao.HibernateUtil;
 import data.dao.TransactionContext;
 import data.entities.UsersJobs;
+import java.util.List;
+import org.hibernate.FetchMode;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
-import org.hibernate.Transaction;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
 import services.exceptions.OcsPersistenceException;
 
-
 public class UserJobRelationServices {
-    
+
     private SessionFactory sessionFactory;
 
-    public UserJobRelationServices( SessionFactory sessionFactory ) {
+    public UserJobRelationServices(SessionFactory sessionFactory) {
         this.sessionFactory = sessionFactory;
     }
-    
-    public UserJobRelationServices( ){
-        this( HibernateUtil.getSessionFactory() );
+
+    public UserJobRelationServices() {
+        this(HibernateUtil.getSessionFactory());
     }
 
-    public void createUserJobRelation( UsersJobs userJob ) {
-        Session session = sessionFactory.getCurrentSession();        
-        
-        try(TransactionContext ctx = new TransactionContext(session)){
+    public void createUserJobRelation(UsersJobs userJob) {
+        Session session = sessionFactory.getCurrentSession();
 
-            session.save( userJob );
+        try (TransactionContext ctx = new TransactionContext(session)) {
+            session.save(userJob);
+            ctx.commit();
+        } catch (HibernateException e) {
+            throw new OcsPersistenceException(e);
+        }
+    }
+    
+    public void updateUserJobRelation(UsersJobs usersJobs) {
+        Session session = sessionFactory.getCurrentSession();
+
+        try (TransactionContext ctx = new TransactionContext(session)) {
+            session.update(usersJobs);
+            ctx.commit();
+        } catch (HibernateException e) {
+            throw new OcsPersistenceException(e);
+        }
+    }
+
+    public void deleteUserJobRelation(UsersJobs usersJobs) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        try (TransactionContext ctx = new TransactionContext(session)) {
+            session.delete(usersJobs);
+            ctx.commit();
+        } catch (HibernateException e) {
+            throw new OcsPersistenceException(e);
+        }
+    }
+
+    public List<UsersJobs> getUsersJobsByUserId(int userId) {
+        Session session = sessionFactory.getCurrentSession();
+
+        try (TransactionContext ctx = new TransactionContext(session)) {
+            List<UsersJobs> result = (List<UsersJobs>) session.createCriteria(UsersJobs.class)
+                    .add(Restrictions.eq("user.id", userId))
+                    .setFetchMode("job", FetchMode.JOIN)
+                    .addOrder(Order.desc("endTime"))
+                    .addOrder(Order.desc("startTime"))
+                    .list();
             ctx.commit();
 
-        }catch (HibernateException e) {
+            return result;
+        } catch (HibernateException e) {
             throw new OcsPersistenceException(e);
-
-        }    
+        }
     }
 
+    public UsersJobs getUsersJobsById(int id) {
+        Session session = sessionFactory.getCurrentSession();
+
+        try (TransactionContext ctx = new TransactionContext(session)) {
+            UsersJobs result = (UsersJobs) session.get(UsersJobs.class, id);
+            ctx.commit();
+
+            return result;
+        } catch (HibernateException ex) {
+            throw new OcsPersistenceException(ex);
+        }
+    }
 }
