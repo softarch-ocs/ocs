cd5ac1329d7a9adcdba5e98449596b40b98a44fe
hernan940730 <hdolarteb@unal.edu.co>
2016-03-06 23:35:28 +0000

    Features Test Cases added with JUNIT

---
+++ scripts/copy.sql
@@ -0,0 +1,8 @@
+INSERT INTO Job SELECT * FROM job;
+INSERT INTO Job_jobFeatures SELECT * FROM  job_jobfeatures;
+INSERT INTO JobArea SELECT * FROM jobarea;
+INSERT INTO JobFeature SELECT * FROM jobfeature;
+INSERT INTO `User` SELECT * FROM `user`;
+INSERT INTO User_jobFeatures SELECT * FROM user_jobfeatures;
+INSERT INTO UsersJobs SELECT * FROM usersjobs;
+INSERT INTO JobRequest SELECT * FROM jobrequest;
