0993607291aa00bb473c6427aa5d175fec4290d4
Daniel Castro <dancas2@gmail.com>
2016-03-06 18:26:27 -0500

    Adding infrastructure to run functional tests from Java

---
+++ test/functional/FunctionalTestBase.java
@@ -0,0 +1,59 @@
+package functional;
+
+import java.util.concurrent.TimeUnit;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+@RunWith(JUnit4.class)
+public class FunctionalTestBase {
+    protected WebDriver driver;
+    protected String baseUrl;
+    
+    @Rule
+    public KnownDatabaseState state = new KnownDatabaseState();
+
+    @Before
+    public void setUp() throws Exception {
+        driver = new FirefoxDriver();
+        baseUrl = "http://localhost:8080/ocs/";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+    
+    protected void loginAs(String userName, String password) {
+        driver.findElement(By.linkText("Login")).click();
+        driver.findElement(By.id("j_idt18:inputEmail")).clear();
+        driver.findElement(By.id("j_idt18:inputEmail")).sendKeys(userName);
+        driver.findElement(By.id("j_idt18:inputPassword")).clear();
+        driver.findElement(By.id("j_idt18:inputPassword")).sendKeys(password);
+        driver.findElement(By.name("j_idt18:j_idt22")).click();
+    }
+    
+    protected void loginAsSampleUser() {
+        loginAs("ah@ah.com", "ah@ah.com");
+    }
+    
+    protected void loginAsSampleAdmin() {
+        loginAs("admin@ocs.com", "admin@ocs.com");
+    }
+
+    protected boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+}
---
+++ test/functional/KnownDatabaseState.java
@@ -0,0 +1,44 @@
+package functional;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import org.junit.rules.ExternalResource;
+
+public class KnownDatabaseState extends ExternalResource {
+    private static final String CONNECTION_STRING = 
+            "jdbc:mysql://localhost:3306/ocs?allowMultiQueries=true";
+    private static final String USER_NAME = "root";
+    private static final String PASSWORD = "";
+    private static final String BACKUP_FILE = 
+            "functional_tests/test_database2.sql";
+    
+    private static String cachedBackupFile;
+
+    @Override
+    protected void before() throws Throwable {
+        super.before(); 
+        
+        long time = System.currentTimeMillis();
+        try(Connection connection = 
+                DriverManager.getConnection(CONNECTION_STRING, USER_NAME, PASSWORD)) {
+            connection.setAutoCommit(false);
+            connection.createStatement().execute(getBackupSql());
+            connection.commit();
+        }
+        time = System.currentTimeMillis() - time;
+        System.out.println("Restoring DB took " + time + "ms");
+    }
+    
+    private synchronized String getBackupSql() throws IOException {
+        if (cachedBackupFile == null) {
+            cachedBackupFile = new String(Files.readAllBytes(Paths.get(BACKUP_FILE)), 
+                StandardCharsets.UTF_8);
+        }
+        
+        return cachedBackupFile;
+    }
+}
