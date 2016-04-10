b92b4f80be6f22336a223672577273c24b3c0fcb
Felipe Cruz <bones.felipe@gmail.com>
2016-04-10 01:55:25 -0500

    Enabling replication for ShowJobController

---
+++ src/presentation/beans/ShowJobsBean.java
@@ -0,0 +1,21 @@
+package presentation.beans;
+
+import data.entities.Job;
+import java.io.Serializable;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class ShowJobsBean implements Serializable{
+    private List<Job> jobs;
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
+}
