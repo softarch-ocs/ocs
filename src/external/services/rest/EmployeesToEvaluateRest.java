2b3c41d9cf64ae25d45e458d0216d0b89b799bb1
afcruzs <bones.felipe@gmail.com>
2016-04-17 21:00:26 -0500

    Added rest service

---
+++ src/external/services/rest/EmployeesToEvaluateRest.java
@@ -0,0 +1,106 @@
+
+package external.services.rest;
+
+import data.entities.JobFeature;
+import data.entities.JobRequest;
+import data.entities.User;
+import external.dto.EmployeeToEvaluateDto;
+import external.dto.EmployeesToEvaluateDto;
+import external.services.soap.UnauthorizedException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
+import javax.xml.ws.WebServiceException;
+import services.UserService;
+import services.jobs.JobRequestService;
+
+
+@Path("rest/employeesToEvaluate")
+public class EmployeesToEvaluateRest {
+
+    @Context
+    private UriInfo context;
+    
+    private JobRequestService jobRequestService;
+    private UserService userService;
+
+    public EmployeesToEvaluateRest(JobRequestService jobRequestService, UserService userService) {
+        this.jobRequestService = jobRequestService;
+        this.userService = userService;
+        
+    }
+   
+
+    public EmployeesToEvaluateRest() {
+        this(new JobRequestService(), new UserService());
+    }
+
+    /**
+     * Retrieves representation of an instance of external.services.rest.EmployeesToEvaluateRest
+     * @param userName
+     * @param password
+     * @return an instance of external.dto.EmployeesToEvaluateDto
+     * @throws external.services.soap.UnauthorizedException
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public EmployeesToEvaluateDto getJson(
+            @HeaderParam("X-Username") String userName,
+            @HeaderParam("X-Password") String password
+            ) {
+               
+        
+        User user = userService.getUserByEmailAndPassword(userName, password);
+        
+        if (user == null || !user.hasRole(User.Role.WEB_SERVICE)) {
+            throw new WebApplicationException(Response.Status.UNAUTHORIZED);
+        }
+        
+        EmployeesToEvaluateDto result = new EmployeesToEvaluateDto();
+        
+        List<JobRequest> requests = 
+                jobRequestService.readActiveJobRequests();
+        List<EmployeeToEvaluateDto> employees = new ArrayList<>();
+        
+        for(JobRequest request : requests) {
+            EmployeeToEvaluateDto employeeDto = new EmployeeToEvaluateDto();
+            
+            List<String> features = new ArrayList<>();
+            for(JobFeature feature : request.getUser().getJobFeatures()) {
+                features.add(feature.getName());
+            }
+            
+            employeeDto.setDocument(request.getUser().getPersonalId());
+            employeeDto.setFeatures(features);
+            employees.add(employeeDto);
+        }
+        
+        result.setEmployees(employees);
+        
+        return result;
+    }
+
+    /**
+     * PUT method for updating or creating an instance of EmployeesToEvaluateRest
+     * @param content representation for the resource
+     */
+    /* @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void putJson(EmployeesToEvaluateDto content) {
+    }
+    */
+}
+
